a6cf3c9fcea0bb2b09a46aa6a71024d24642311f
alexandre-vidal <alexandre.vidal@laplateforme.io>
2023-11-21 11:30:54 +0100

    Corrections multiplications

--- jour02/job04/multiplications.py
+++ jour02/job04/multiplications.py
@@ -1,5 +1,5 @@
 n=int(input("Saisissez un entier supérieur à 0 : "))
-if (n) > 0:
+if (n) > int(0):
     print("Table de multiplication de", (n), ":")
     for x in range(1, 11):
         print((n), "x", (x), "=", (n*x))
