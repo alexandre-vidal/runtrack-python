b3dbc7228d58a136f1d20d0ba5fce911a3f819f6
alexandre-vidal <alexandre.vidal@laplateforme.io>
2023-11-21 14:40:02 +0100

    Job 05

--- jour02/job05/fizzbuzz.py
+++ jour02/job05/fizzbuzz.py
@@ -1,2 +1,8 @@
-for x in range(101):
-    print(x)
+for x in range(1, 101):
+    if x%3==0 and x%5==0:
+        print("FizzBuzz")
+    elif x%3==00:
+        print("Fizz")
+    elif x%5==00:
+        print("Buzz")
+    else: print(x)
