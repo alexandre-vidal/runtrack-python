c81d4292d4868af80715be5b58552bbadbc8aba8
Alexandre Vidal <alexandre.vidal@laplateforme.io>
2024-01-10 16:47:07 +0100

    Mise à jour

--- jour03/job05/main.py
+++ jour03/job05/main.py
@@ -1,7 +1,10 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-op = input("Enter mathematical operator: ")
-if op in ('+', '-', '*', '/' , '//', '%', '**'):
-    print("Result is", eval(f"{num1}{op}{num2}"))
-else:
-    print("Operator not recognized.")
+def Calcule():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    op = input("Enter mathematical operator: ")
+    if op in ('+', '-', '*', '/' , '//', '%', '**'):
+        print("Result is", eval(f"{num1}{op}{num2}"))
+    else:
+        print("Operator not recognized.")
+        
+Calcule()
--- jour03/job06/main.py
+++ jour03/job06/main.py
@@ -1,2 +1,8 @@
 def positifnegatif(nombre):
-    
+    if nombre > 0:
+        print(nombre, "est positif")
+    elif nombre < 0:
+        print(nombre, "est négatif")
+        
+positifnegatif(1)
+positifnegatif(-1)
